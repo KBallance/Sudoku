e1967006608dd1332e2c0279bfa0182273b312c1
Lewis Turner <lewis.turner.2016@gmail.com>
2025-10-02 11:44:28 +0100

    added debug & hard test

--- sudoku.py
+++ sudoku.py
@@ -6,27 +6,43 @@ import time
 class csp():
     #constructor variables initail puzzle state is passed in
     def __init__(self, variables):
+        #debug
+        self.calls = {
+            "makeDomains": 0,
+            "makeConstraints": 0,
+            "selectUnasignedVar": 0,
+            "isConsistent": 0,
+            "orderDomainValues": 0,
+            "recursiveBacktrack": 0
+        }
+
         self.variables = [(i,j) for i in range(9) for j in range(9)]
         self.domains = self.makeDomains(variables)
-        self.constraints = {}
-        self.makeConstraints(self.variables)
+        self.constraints = self.makeConstraints(self.variables)
+    
+    def printCalls(self):
+        for k,v in self.calls.items():
+            print(f"{k}: {v}")
 
     #makes domains for each variable stored as dictionary
     #for variables that dont have an initial assignment a list of 1-9 is given
     #else domain is set to [assignment]
     def makeDomains(self, variables):
+        self.calls["makeDomains"] += 1
         domains = {}
         for x in range(9):
             for y in range(9):
                 if variables[x][y] != 0:
                     domains[(x,y)] = [variables[x][y]]
                 else:
-                    domains[(x,y)] = [i for i in range(1,10)]
+                    domains[(x,y)] = [*range(1,10)]
         
         return domains
     
-    #makes constaraints for each value stored in dictionary
+    #makes constraints for each value stored in dictionary
     def makeConstraints(self, variables):
+        self.calls["makeConstraints"] += 1
+        constraints = {}
         for var in variables:
             constraint = []
             for i in range(9):
@@ -39,15 +55,17 @@ class csp():
             
             subX, subY = var[0] // 3, var[1] // 3
 
-            for x in range(subX * 3, (subX+1) *3):
-                for y in range(subY *3, (subY+1)*3):
+            for x in range(subX * 3, (subX + 1) * 3):
+                for y in range(subY * 3, (subY + 1) * 3):
                     if (x,y) != var:
                         constraint.append((x,y))
             
-            self.constraints[var] = constraint
+            constraints[var] = constraint
+        return constraints
     
     #select next variable that has not been given an asignment yet
     def selectUnasignedVar(self, assignment):
+        self.calls["selectUnasignedVar"] += 1
         unassignedVar = []
         for var in self.variables: 
             if var not in assignment:
@@ -60,13 +78,14 @@ class csp():
                 return var1
             #finds min domain var
             else:
-                if len(self.domains[var1]) < len(self.domains[var1]):
+                if len(self.domains[var1]) < len(self.domains[curMin]):
                     curMin = var1
         return curMin
 
 
     #checks if value assigned conforms to teh variables constraints
     def isConsistent(self, var, value, assignment):
+        self.calls["isConsistent"] += 1
         for constraint in self.constraints[var]:
             if constraint in assignment and assignment[constraint] == value:
                 return False
@@ -74,6 +93,7 @@ class csp():
 
     #gets the pomain value sof a variable
     def orderDomainValues(self, var):
+        self.calls["orderDomainValues"] += 1
         return self.domains[var]
     
     #starts search 
@@ -82,6 +102,7 @@ class csp():
 
     #does search
     def recursiveBacktrack(self, assignment):
+        self.calls["recursiveBacktrack"] += 1
         #checks if every var as been assigned
         if len(assignment) == len(self.variables):
             return assignment
@@ -89,7 +110,7 @@ class csp():
         #gets next var
         var = self.selectUnasignedVar(assignment)
 
-        #gets first val that corforms to constraints else returns fail(None) as we want to return a complete puzzel not a success or fail
+        #gets first val that conforms to constraints else returns fail(None) as we want to return a complete puzzle not a success or fail
         for val in self.orderDomainValues(var):
             if self.isConsistent(var, val, assignment):
                 assignment[var] = val
@@ -130,25 +151,38 @@ def printSudoku(puzzle):
 def main():
     #create csp object, passing initial state 
 
-    #problem = csp([[7,0,0,0,9,2,0,5,0],
-    #            [0,3,0,1,7,5,6,0,0],
-    #            [9,0,1,8,4,0,2,7,0],
-    #            [3,7,0,0,5,0,0,1,0],
-    #            [5,4,0,9,0,0,7,3,6],
-    #            [0,6,0,0,0,0,0,0,5],
-    #            [6,2,0,7,0,9,0,4,8],
-    #            [8,1,0,0,0,0,9,0,0],
-    #            [0,0,7,2,0,0,5,0,1]])
+    problem = csp([[7,0,0,0,9,2,0,5,0],
+               [0,3,0,1,7,5,6,0,0],
+               [9,0,1,8,4,0,2,7,0],
+               [3,7,0,0,5,0,0,1,0],
+               [5,4,0,9,0,0,7,3,6],
+               [0,6,0,0,0,0,0,0,5],
+               [6,2,0,7,0,9,0,4,8],
+               [8,1,0,0,0,0,9,0,0],
+               [0,0,7,2,0,0,5,0,1]])
         
-    problem = csp([[0,0,0,0,0,0,0,0,0],
-                   [0,0,0,0,0,0,0,0,0],
-                   [0,0,0,0,0,0,0,0,0],
-                   [0,0,0,0,0,0,0,0,0],
-                   [0,0,0,0,0,0,0,0,0],
-                   [0,0,0,0,0,0,0,0,0],
-                   [0,0,0,0,0,0,0,0,0],
-                   [0,0,0,0,0,0,0,0,0],
-                   [0,0,0,0,0,0,0,0,0]])
+    # problem = csp([[0,0,0,0,0,0,0,0,0],
+    #                [0,0,0,0,0,0,0,0,0],
+    #                [0,0,0,0,0,0,0,0,0],
+    #                [0,0,0,0,0,0,0,0,0],
+    #                [0,0,0,0,0,0,0,0,0],
+    #                [0,0,0,0,0,0,0,0,0],
+    #                [0,0,0,0,0,0,0,0,0],
+    #                [0,0,0,0,0,0,0,0,0],
+    #                [0,0,0,0,0,0,0,0,0]])
+
+    #hard from telegram
+    # problem = csp([
+    #     [7,0,0,8,0,0,4,0,1],
+    #     [0,0,9,0,5,0,0,0,0],
+    #     [0,0,0,0,0,0,0,9,0],
+    #     [0,0,0,0,2,0,3,0,0],
+    #     [0,1,0,4,0,0,0,6,0],
+    #     [6,4,0,0,7,8,0,0,0],
+    #     [0,0,0,0,0,0,0,5,0],
+    #     [0,3,0,2,0,7,8,0,0],
+    #     [0,8,0,0,0,0,2,0,0]
+    # ])
 
     startT = time.perf_counter_ns()
 
@@ -161,16 +195,14 @@ def main():
 
     #convert to 2D array for easy value access
     #also so that we dont have to destroy the dictionary to get random access
-    solution = [[0 for _ in range(9)] for _ in range(9)]
+    solution = [[0] * 9 for _ in range(9)]
     for (i,j), val in sol.items():
         solution[i][j] = val
-    
-
-
                 
     printSudoku(solution)
 
     print(f"runtime (ms): {runtime}")
+    problem.printCalls()
 
 
 main()
