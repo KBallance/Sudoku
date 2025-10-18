05663045a894983c0f217e10c6dfa14272d61f0a
Lewis <lewis.turner.2016@gmail.com>
2025-10-18 01:24:09 +0100

    added more comments

--- sudoku.py
+++ sudoku.py
@@ -78,6 +78,7 @@ class csp():
         self.gui = self.createGui() 
 
     def createGui(self):
+        #adds padding to middle squares (both axis)
         def genPadding(x):
             smallGap, bigGap = 0, 1
             if x == 1:
@@ -89,46 +90,56 @@ class csp():
         #used over simple entry widgets as they dont look very nice     
         editManager = EditLabel(self)
 
+        #window init
         root = tk.Tk()
         root.resizable(False, False)
         root.title("Sudoku Solver")
         mainWindow = tk.Frame(root, height=120, width=100, bg=hexFromRgb(50,50,50))
         mainWindow.grid(column=0,row=0)
 
+        #heading
         titleFont = tkFont.Font(family="Verdana", size=20, weight="bold")
         title = tk.Label(mainWindow, text="Sudoku Solver", bg=hexFromRgb(50,50,50), height=2, width=15, font=titleFont, foreground="White")
         title.grid(column=1, row=0, columnspan=3)
 
+        #grid init
         sudokuFrame = tk.Frame(mainWindow, bg=hexFromRgb(255,255,255))
         sudokuFrame.grid(column=1, row=1)
-
+        #use sub frames to get around grid padding limitations
         subFrames = [[tk.Frame]*3 for _ in range(3)]
         for x in range(len(subFrames)):
             for y in range(len(subFrames)):
+                #sub frame init
                 subFrames[x][y] = tk.Frame(sudokuFrame, bg=hexFromRgb(255,255,255))
                 subFrames[x][y].grid(row=x, column=y, padx=genPadding(x), pady=genPadding(y))
 
+        #add cells to grid
         cellFont = tkFont.Font(family="Verdana", size=12, weight="bold")
         for i in range(9):
             for j in range(9):
+                #cell init
                 self.cellsStrVar[i][j] = tk.StringVar(mainWindow)
                 self.cells[i][j] = tk.Label(subFrames[i//3][j//3], name=f"{i}{j}", bg=hexFromRgb(120,120,120), width=5, height=2, cursor="ibeam" ,foreground="white", anchor="center", font=cellFont, textvariable=self.cellsStrVar[i][j])
                 self.cells[i][j].bind("<Button-1>", editManager.swapCell)
+                self.cells[i][j].grid(row=i, column=j, padx=1, pady=1)
                 
+                #set cell value if input cell != 0
                 if len(self.domains[i,j]) == 1:
                     self.cells[i][j].config(bg=hexFromRgb(75,75,75))
                     self.cellsStrVar[i][j].set(f"{self.domains[i,j][0]}")
-                
-                self.cells[i][j].grid(row=i, column=j, padx=1, pady=1)
         
+        #add event for click
         root.bind("<Key>", editManager.edit)
         
+        #button frame init
         buttonFrame = tk.Frame(mainWindow, bg=hexFromRgb(50,50,50), height = 4)
         buttonFrame.grid(row=3, column=1, columnspan = 3)
 
+        #solve btn
         solveButton = tk.Button(buttonFrame, bg=hexFromRgb(180,180,180), relief="ridge", text="Solve Puzzle", font=cellFont, fg = hexFromRgb(25,25,25), height=2, width=12, command=self.showSolve)
         solveButton.grid(row=1, column=1, columnspan=1, pady=2, padx=2)
-
+        
+        #clear btn
         clearButton = tk.Button(buttonFrame, bg=hexFromRgb(180,180,180), relief="ridge", text="Clear Puzzle", font=cellFont, fg = hexFromRgb(25,25,25), height=2, width=12, command=self.clearPuzzle)
         clearButton.grid(row=1, column=2, columnspan=1, pady=2, padx=2)
 
@@ -136,16 +147,17 @@ class csp():
 
     #solve puzzle and show on gui
     def showSolve(self):
-        self.clearCalls()
+        if args.gui: self.clearCalls()
 
+        #solve & track time taken
         startT = time.perf_counter_ns()
         self.solution = self.backtrackSearch()
-        endT = time.perf_counter_ns()
+        runtime = (time.perf_counter_ns()-startT)/1e6
 
-        for (i,j), val in self.solution.items():
-            self.cellsStrVar[i][j].set(str(val))
-        
-        runtime = (endT-startT)/(1e6)
+        #update gui cells
+        if args.gui:
+            for (i,j), val in self.solution.items():
+                self.cellsStrVar[i][j].set(str(val))
         
         #debug
         if not args.gui or args.debug: self.printSudoku(self.solution)
@@ -153,17 +165,18 @@ class csp():
             print(f"runtime: {runtime}ms")
             self.printCalls()
     
+    #clear gui cells
     def clearPuzzle(self):
         for i in range(9):
             for j in range(9):
                 if self.cells[i][j].cget("bg") != hexFromRgb(75,75,75):
                     self.cellsStrVar[i][j].set('')
-
     
+    #wrapper fun for tkinter mainloop
     def showGui(self):
         self.gui.mainloop()
 
-    #prints some data on number of function calls
+    #prints some debug data on number of function calls
     def printCalls(self):
         for k,v in self.calls.items():
             print(f"{k}: {v}")
@@ -185,7 +198,7 @@ class csp():
     def updateDomains(self):
         for i in range(9):
             for j in range(9):
-                t = self.cells[i][j].cget("text")
+                t = self.cellsStrVar[i][j].get()
                 if t == '':
                     self.domains[i,j] =  {1,2,3,4,5,6,7,8,9}
                 else:
@@ -193,9 +206,8 @@ class csp():
     
     #clears call values between solves
     def clearCalls(self):
-        for k , _ in self.calls.items():
+        for k in self.calls.keys():
             self.calls[k] = 0
-
 
     #make domains from inital puzzle
     def makeDomains(self, variables):
@@ -207,7 +219,6 @@ class csp():
                     domains[(x,y)] = [variables[x][y]]
                 else:
                     domains[(x,y)] = [*range(1,10)]
-        
         return domains
     
     #makes constraints for each value stored in dictionary
@@ -296,17 +307,6 @@ class csp():
 def hexFromRgb(r,g,b):
     return f'#{r:02x}{g:02x}{b:02x}'
 
-#print the solution in a nice format
-def printSudoku(puzzle):
-    for i in range(9):
-        if i%3 ==0 and i !=0:
-            print("- - - - - - - - - - -")
-        for j in range(9):
-            if j%3 == 0 and j != 0:
-                print("|", end= " ")
-            print(puzzle[i][j], end= " ")
-        print()
-
 #check file arg is .txt or .csv (case insensitive)
 def isFileType(string:str):
     try:
@@ -315,7 +315,7 @@ def isFileType(string:str):
             return string
         else:
             raise TypeError
-    except Exception as e:
+    except:
         raise
 
 #create parser for command line args
@@ -359,8 +359,9 @@ def readPuzzle(filename):
 
     return csp(vars)
 
+#runs only if file directly run (not imported by another file)
 if __name__ == "__main__":
-    args = parser.parse_args() #get args from command line
+    args = parser.parse_args() #get args from command line (uses sys package by default)
 
     problem = readPuzzle(args.filename)
     if args.gui:
